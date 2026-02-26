06bab6a933e8270e3a6074c5aad748bfaedbcd1f
pdbice <pdbice@gmail.com>
2026-02-26 04:19:03 -0500

    Add test folder

---
+++ test/tokenizer_syntax_error.s
@@ -0,0 +1,5 @@
+.BYTR 0		; Unknown directive
+MOV V0, -100	; Invalid unsigned integer
+MOV V1, 256	; Value is out of range 0..255
+.ASCII "Error	; Missing " in string literal
+!		; Unexpected character
