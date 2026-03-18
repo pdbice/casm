aa03e5936522c33aea1d9cc80543f6e990a25614
pdbice <pdbice@gmail.com>
2026-03-18 04:41:55 -0400

    Remove tests and examples

--- test/directive_errors.s
+++
@@ -1,2 +0,0 @@
-.BYTE 0 1 2 _Unexpected			; Line 1: Expected unsigned integer, got Label
-.ASCII "String literal" _Unexpected	; Line 2: Expected string, got Label
--- test/tokenizer_errors.s
+++
@@ -1,5 +0,0 @@
-.BYTR 0		; Line 1: Unknown directive .BYTR
-MOV V0, -100	; Line 2: Invalid unsigned integer -100
-MOV V1, 256	; Line 3: Value 256 is out of range 0..255
-.ASCII "Error	; Line 4: Missing " in string literal
-!		; Line 5: Unexpected character !
